16e251ea72f158732714a8a08f5f0a09ee82b82a
Nathan Kimutai <nathanpykimutai@gmail.com>
2025-04-17 07:43:21 +0300

    fix: hard code the sender

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     BLOGGING_MAIL_PREFIX = '[Flasky]'
     BLOGGING_ADMIN_EMAIL = os.environ.get('MAIL_ADMIN')
-    BLOGGING_MAIL_SENDER = os.environ.get('MAIL_SENDER')
+    BLOGGING_MAIL_SENDER = "nathanpykimutai@gmail.com" 
     BLOGGING_ADMIN =  os.environ.get('MAIL_ADMIN')
     BLOGGING_MAIL_SUBJECT_PREFIX = "[Flasky]"
     BLOGGING_POSTS_PER_PAGE = 5

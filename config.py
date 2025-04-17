0e49625697b00974adc92c859352bf0ab9afb90d
Nathan Kimutai <nathanpykimutai@gmail.com>
2025-04-17 07:37:02 +0300

    fix: change some config vars

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hardtoguessstring'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    BLOGGING_MAIL_PREFIX = '[Blogging]'
+    BLOGGING_MAIL_PREFIX = '[Flasky]'
     BLOGGING_ADMIN_EMAIL = os.environ.get('MAIL_ADMIN')
     BLOGGING_MAIL_SENDER = os.environ.get('MAIL_SENDER')
     BLOGGING_ADMIN =  os.environ.get('MAIL_ADMIN')
-    BLOGGING_MAIL_SUBJECT_PREFIX = "[Blogging]"
+    BLOGGING_MAIL_SUBJECT_PREFIX = "[Flasky]"
     BLOGGING_POSTS_PER_PAGE = 5
     BLOGGING_COMMENTS_PER_PAGE = 5
 
